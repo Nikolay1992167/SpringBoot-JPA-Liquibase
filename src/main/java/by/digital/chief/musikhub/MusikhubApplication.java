268d720cc2262aa7908a6eb5ed6b9fbe02c59c7a
Nikolay1992167 <wulik1081@mail.ru>
2023-06-27 10:34:00 +0300

    init commit

---
+++ src/main/java/by/digital/chief/musikhub/MusikhubApplication.java
@@ -0,0 +1,13 @@
+package by.digital.chief.musikhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusikhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/digital/chief/musikhub/MusikhubApplicationTests.java
@@ -0,0 +1,13 @@
+package by.digital.chief.musikhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
